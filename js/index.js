d486f7a9fa1348bbf4881a786fab4e34c5f51b95
Gamerx20003 <you@example.com>
2022-01-26 11:55:35 +0530

    collapsible added

---
+++ js/index.js
@@ -0,0 +1,60 @@
+function myf1() {
+
+    var x = document.getElementById("select1");
+    var y = document.getElementById("select6");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "block";
+    } else {
+    x.style.display = "none";
+    y.style.display = "none";
+    }
+    }
+ function myf2() {
+    var x = document.getElementById("select2");
+    var y = document.getElementById("select5");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "block";
+    } else {
+    x.style.display = "none";
+    y.style.display = "none";
+    }
+    } 
+ function myf3() {
+    var x = document.getElementById("select3");
+    var y = document.getElementById("select4");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "block";
+    } else {
+    x.style.display = "none";
+    y.style.display = "none";
+    }
+    } 
+ function mygame() {
+    var x = document.getElementById("gamecontent");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+    }
+ function myanime() {
+    var x = document.getElementById("animecontent");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+    }
+ function mycode() {
+    var x = document.getElementById("codecontent");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+    }   
+
+
